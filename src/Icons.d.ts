8d4f40f8847d7cda065fd7b9c919aed4c269d631
Pawel Psztyc <jarrodek@gmail.com>
2022-04-11 16:49:38 -0700

    fix: adding icon types
    Signed-off-by: Pawel Psztyc <jarrodek@gmail.com>

---
+++ src/Icons.d.ts
@@ -0,0 +1,12 @@
+import { SVGTemplateResult } from 'lit';
+
+/**
+ * Wraps icon into an SVG container.
+ * @param tpl Icon definition
+ * @param width The icon view box width
+ * @param height The icon view box height
+ * @returnsComplete SVG icon definition
+ */
+export function iconWrapper(tpl: SVGTemplateResult, width?: number, height?: number): SVGTemplateResult;
+export const arrowRight: SVGTemplateResult;
+export const check: SVGTemplateResult;
