310ff342a107aabdd94aadb9ed7cc1ed569f4f44
Pawel Psztyc <jarrodek@gmail.com>
2022-06-11 13:40:48 -0700

    chore: updating CSS for the menu items
    Signed-off-by: Pawel Psztyc <jarrodek@gmail.com>

--- src/styles/MenuElementStyles.js
+++ src/styles/MenuElementStyles.js
@@ -9,20 +9,20 @@ export const MenuElementStyles = css`
   user-select: none;
   z-index: 100;
   --anypoint-item-icon-width: 40px;
-  --anypoint-item-min-height: 40px;
-  --anypoint-item-padding: 0px 24px;
 }
 
 .listbox {
-  padding: 2px 0;
-  min-width: 160px;
-  border-radius: 4px;
+  padding: var(--context-menu-padding, 2px 0);
+  min-width: var(--context-menu-min-width, 240px);
+  border-radius: var(--context-menu-border-radius, 4px);
   box-shadow: var(--context-menu-shadow, var(--anypoint-dropdown-shadow));
   background-color: var(--context-menu-background-color, var(--primary-background-color));
 }
 
 .item {
-  margin: 8px 0;
+  margin: var(--context-menu-item-margin, 8px 0);
+  padding: var(--context-menu-item-padding, 0px 24px);
+  min-height: var(--context-menu-item-min-height, 40px);
 }
 
 .item.disabled {
@@ -38,19 +38,21 @@ export const MenuElementStyles = css`
 .menu-divider {
   height: 1px;
   background-color: var(--context-menu-divider-color, rgba(0, 0, 0, 0.12));
-  margin: 8px 0 8px 40px;
+  margin: var(--context-menu-divider-color, 8px 0 8px 64px);
 }
 
 .menu-section-label {
   font-size: var(--context-menu-section-label-font-size, 0.9rem);
   font-weight: var(--context-menu-section-label-font-weight, 500);
-  margin: 16px 8px 8px 44px;
+  margin: var(--context-menu-section-label-margin, 16px 8px 8px 64px);
   text-transform:  var(--context-menu-section-label-text-transform, uppercase);
 }
 
 .menu-icon {
   width: 20px;
   height: 20px;
+  min-width: 20px;
+  min-height: 20px;
   fill: var(--context-menu-icon-color, currentColor);
 }
 
@@ -67,5 +69,12 @@ export const MenuElementStyles = css`
   width: 100%; 
   height: 100%; 
   fill: currentColor;
+}
+
+.menu-label {
+  display: block;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 }
 `;
